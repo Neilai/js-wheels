59326b0ad2402415fdc1b389477ff1815c11d02a
Neil <laijingzhi@foxmail.com>
2019-11-14 17:36:00 +0800

    add di

--- list-diff/diff-1.js
+++ list-diff/diff-1.js
@@ -2,7 +2,6 @@ var diff = function(oldList, newList) {
     var changes = [];
     // 镜像，模拟位置
     var _oldList = oldList.slice();
-    // 遍历旧的，找出 删
     oldList.forEach(function(item, i) {
         if (newList.indexOf(item) === -1) {
             changes.push({
@@ -12,12 +11,9 @@ var diff = function(oldList, newList) {
             _oldList.splice(i, 1);
         }
     });
-
-    // 遍历新的，找出 增/移
     newList.forEach(function(item, i) {
         var index = _oldList.indexOf(item);
         if (index === -1) {
-            // 增
             changes.push({
                 type: 'insert',
                 index: i,
@@ -26,7 +22,6 @@ var diff = function(oldList, newList) {
             _oldList.splice(i, 0, item);
         }
         else {
-            // 移
             if (index !== i) {
                 var step = {
                     type: 'move',
@@ -38,7 +33,6 @@ var diff = function(oldList, newList) {
             }
         }
     });
-
     return changes;
 };
 var move = function(list, from, to) {
